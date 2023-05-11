e25fbe7de591f40fcaf5490e126c94eef4f1d0e2
mariamaks1301 <mariamaks1301@gmail.com>
2023-05-11 23:35:08 +0600

    add redux-persist to save in local-storage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 
-import {store} from './store'
+import {store, persistor} from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+              <App />
+          </PersistGate>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -1,11 +1,44 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import { 
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER, } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { positionReducer } from "./features/positions/position-slice";
 import { filterReducer } from "./features/filter/filter-slice";
 
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+const rootReducer = combineReducers({
+    filters: filterReducer,
+    positions: positionReducer,
+})
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+
 export const store = configureStore({
-    reducer: {
-        filters: filterReducer,
-        positions: positionReducer
-    },
+    reducer: persistedReducer,
     devTools: true,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
